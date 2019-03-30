3d543ea094112322c062e1e79f191cbdb8103eab
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2019-03-30 16:17:27 -0400

    Possibly a cloud function

--- backend_node/index.js
+++ backend_node/index.js
@@ -1,53 +1,64 @@
 "use strict";
-const { loadImage, createCanvas } = require("canvas");
+const { loadImage, createCanvas, Image } = require("canvas");
 const tf = require("@tensorflow/tfjs");
 require("@tensorflow/tfjs-node"); // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
 
 const SKIN_CLASSES = {
-  0: 'akiec, Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowenâ€™s disease)',
-  1: 'bcc, Basal Cell Carcinoma',
-  2: 'bkl, Benign Keratosis',
-  3: 'df, Dermatofibroma',
-  4: 'mel, Melanoma',
-  5: 'nv, Melanocytic Nevi',
-  6: 'vasc, Vascular skin lesion'
+  0: "akiec, Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowenâ€™s disease)",
+  1: "bcc, Basal Cell Carcinoma",
+  2: "bkl, Benign Keratosis",
+  3: "df, Dermatofibroma",
+  4: "mel, Melanoma",
+  5: "nv, Melanocytic Nevi",
+  6: "vasc, Vascular skin lesion"
 };
 
-loadImage("./samplepic.jpg").then(async function(image) {
-  const model = await tf.loadLayersModel(
-    "http://skin.test.woza.work/final_model_kaggle_version1/model.json"
-  );
+let _model = null;
+async function getModel() {
+  if (!_model) {
+    _model = await tf.loadLayersModel(
+      "http://skin.test.woza.work/final_model_kaggle_version1/model.json"
+    );
+  }
+  return _model;
+}
 
-  const canvas = createCanvas(image.width, image.height);
-  const ctx = canvas.getContext("2d");
+exports.fct = async (req, res) => {
+  const base64 = req.body.image;
+  if (!base64) {
+    return []
+  }
+  const model = await getModel();
+  loadImage('data:image/jpg;base64,' + base64).then(async image => {
+    const canvas = createCanvas(image.width, image.height);
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0);
 
-  ctx.drawImage(image, 0, 0);
-
-  let tensor = tf.browser
+    let tensor = tf.browser
     .fromPixels(canvas)
     .resizeNearestNeighbor([224, 224])
     .toFloat();
 
-  let offset = tf.scalar(127.5);
-  tensor = tensor
-    .sub(offset)
-    .div(offset)
-    .expandDims();
+    let offset = tf.scalar(127.5);
+    tensor = tensor
+      .sub(offset)
+      .div(offset)
+      .expandDims();
 
     let predictions = await model.predict(tensor).data();
 
-    let top5 = Array.from(predictions)
-		.map(function (p, i) { // this is Array.map
-			return {
-				probability: p,
-				className: SKIN_CLASSES[i] // we are selecting the value from the obj
-			};
-			
-		}).sort(function (a, b) {
-			return b.probability - a.probability;
-				
-    }).slice(0, 7);
-    
-    console.log(top5);
-});
+    let sorted = Array.from(predictions)
+      .map(function(p, i) {
+        return {
+          probability: p,
+          className: SKIN_CLASSES[i]
+        };
+      })
+      .sort(function(a, b) {
+        return b.probability - a.probability;
+      });
 
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(sorted));
+  });
+};
